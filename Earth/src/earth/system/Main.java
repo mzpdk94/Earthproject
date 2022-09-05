5af69ce2c9928e95725be0e38d63a8d546643dc8
mzpdk94 <mzpdk94@gmail.com>
2022-09-05 16:33:00 +0530

    chang main .java

--- Earth/src/earth/system/Main.java
+++ Earth/src/earth/system/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+		System.out.println("hello world");
 		
 		India i=new India();
 		America a=new America();
